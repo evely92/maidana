5edea84be983384336d01f1cf1747945a17bd633
evely92 <115947045+evely92@users.noreply.github.com>
2023-02-03 03:52:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function redireccionf(){
+    location.href="https://www.forvis.com.ar/"
+}
+
+function redireccions(){
+    location.href="https://www.instagram.com/metspaltro/"
+}
+
+function redireccionb(){
+    location.href="https://www.facebook.com/nrc.boveri.5/?locale=es_LA"
+}
+
+function redirecciona(){
+    location.href="https://www.instagram.com/acrilicosatlas/?hl=es"
+}
+
+function redireccionc(){
+    location.href="https://www.obrasmetalicas.com/"
+}
